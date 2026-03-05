21cd6ef101d15a80237e56a71fa6bb49b285bb94
Corentin Junod <corentin.junod@outlook.com>
2026-03-05 10:59:54 +0100

    Added code part 1

--- slides/src/build.sh
+++ slides/src/build.sh
@@ -1,14 +1,3 @@
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=00_00_intro.html" ../../00_00_intro.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=00_01_refresh_git.html" ../../00_01_refresh_git.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_00_refresh_js.html" ../../01_00_refresh_js.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_01_js_modules.html" ../../01_01_js_modules.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_02_js_inheritance.html" ../../01_02_js_inheritance.pdf')
 
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_03_js_promises.html" ../../01_03_js_promises.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_04_js_values_references.html" ../../01_04_js_values_references.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_05_exceptions.html" ../../01_05_exceptions.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_06_js_scopes.html" ../../01_06_js_scopes.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=01_07_js_proxy.html" ../../01_07_js_proxy.pdf')
 
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=02_00_node_vite.html" ../../02_00_node_vite.pdf')
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=02_01_openlayers.html" ../../02_01_openlayers.pdf')
+(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=03_00_code_part1.html" ../../03_00_code.pdf')
