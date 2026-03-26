88f3b61408ff01bb02d51923a02f1075e399c59e
Corentin Junod <corentin.junod@outlook.com>
2026-03-26 07:55:50 +0100

    Lecture 26.03.26

--- slides/src/build.sh
+++ slides/src/build.sh
@@ -15,6 +15,8 @@
 
 #(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=03_00_code.html" ../../03_00_code.pdf')
 
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=04_00_vue.html" ../../04_00_vue.pdf')
+#(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=04_00_vue.html" ../../04_00_vue.pdf')
 
-(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=05_00_giro3D.html" ../../05_00_giro3D.pdf')
+#(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=05_00_giro3D.html" ../../05_00_giro3D.pdf')
+
+(cd ./reveal_js/ ; npm x -c 'decktape -s 1920x1080 reveal "http://127.0.0.1:8080?showNotes=true&file=06_00_design_patterns.html" ../../06_00_design_patterns.pdf')
